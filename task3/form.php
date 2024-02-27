416e73e46a7282178b1c94821cd499cd860ca616
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-02-27 16:40:48 +0300

    Update form.php

--- task3/form.php
+++ task3/form.php
@@ -2,40 +2,40 @@
         <form>
         <label>
           ФИО:<br />
-          <input required name="field-name-1"
+          <input required name="name"
           placeholder="Введите ваше ФИО" />
         </label><br />
         <label>
         Телефон:<br />
-        <input required name="field-phone"
+        <input required name="phone"
           type="tel"
           placeholder="Введите ваш телефон" />
       </label><br />
       <label>
         Email:<br />
-        <input required name="field-email"
+        <input required name="email"
           type="email"
           placeholder="Введите вашу почту" /></label>
           <label>
             <br />
             Дата рождения:<br />
-            <input name="field-date"
+            <input name="date"
               value="2023-09-14"
               type="date" />
           </label><br />
           Пол:<br />
       <label><input type="radio" checked="checked"
-        name="radio-group-1" value="Значение1" />
+        name="pol" value="Значение1" />
         Мужской</label>
       <label><input type="radio"
         name="radio-group-1" value="Значение2" />
         Женский</label><br />
         <label>
           Биография:<br />
-          <textarea name="field-name-2" placeholder="Ваша биография" ></textarea>
+          <textarea name="bio" placeholder="Ваша биография" ></textarea>
         </label><br />
       <label><input required type="checkbox" checked="checked"
-        name="check-1" />
+        name="ok" />
         С контрактом ознакомлен(а)</label><br />
         <br />
         <input type="submit" value="Сохранить" />
