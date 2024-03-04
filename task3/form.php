f87e6b0f50ba0212f80d33d498e5eb8973a6b789
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-03-04 16:23:13 +0300

    Update form.php

--- task3/form.php
+++ task3/form.php
@@ -1,42 +1,12 @@
-<p style="font-size: 30px; color: rgb(50, 100, 255)">Форма</p>
-        <form action="" method="POST">
-        <label>
-          ФИО:<br />
-          <input name="name"
-          placeholder="Введите ваше ФИО" />
-        </label><br />
-        <label>
-        Телефон:<br />
-        <input required name="phone"
-          type="tel"
-          placeholder="Введите ваш телефон" />
-      </label><br />
-      <label>
-        Email:<br />
-        <input required name="email"
-          type="email"
-          placeholder="Введите вашу почту" /></label>
-          <label>
-            <br />
-            Дата рождения:<br />
-            <input name="date"
-              value="2000-01-01"
-              type="date" />
-          </label><br />
-          Пол:<br />
-      <label><input type="radio" checked="checked"
-        name="pol" value="M" />
-        Мужской</label>
-      <label><input type="radio"
-        name="pol" value="W" />
-        Женский</label><br />
-        <label>
-          Биография:<br />
-          <textarea name="bio" placeholder="Ваша биография" ></textarea>
-        </label><br />
-      <label><input required type="checkbox" checked="checked"
-        name="ok" />
-        С контрактом ознакомлен(а)</label><br />
-        <br />
-        <input type="submit" value="Сохранить" />
-      </form>
+<form action="" method="POST">
+  <input name="fio" />
+  <select name="year">
+    <?php 
+    for ($i = 1922; $i <= 2022; $i++) {
+      printf('<option value="%d">%d год</option>', $i, $i);
+    }
+    ?>
+  </select>
+  
+  <input type="submit" value="ok" />
+</form>
