54f7df60ea0459a4f0e36c68e514a6d1725583f2
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-04-08 17:09:38 +0300

    Update index.php

--- task4/index.php
+++ task4/index.php
@@ -26,32 +26,45 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
   // Складываем признак ошибок в массив.
   $errors = array();
-  if (empty($_POST['name'])) {
+  if (empty($_COOKIE['name_error'])) {
    // print('Заполните имя.<br/>');
     $errors[name_empty] = TRUE;
-} elseif (!preg_match('/^[\p{L}\s]+$/u', $_POST['name'])) {
+} elseif (!preg_match('/^[\p{L}\s]+$/u', $_COOKIE['name_error'])) {
     //print('Имя может содержать только буквы и пробелы.<br/>');
     $errors[name_str] = TRUE;
-} elseif (strlen($_POST['name']) > 150) {
+} elseif (strlen($_COOKIE['name_error']) > 150) {
     //print('Имя '.$_POST['name'].' не должно превышать 150 символов.<br/>');
     $errors[name_len] = TRUE;
 }
 
-  $errors['fio'] = !empty($_COOKIE['fio_error']);
+  //$errors['fio'] = !empty($_COOKIE['fio_error']);
   // TODO: аналогично все поля.
 
   // Выдаем сообщения об ошибках.
-  if ($errors['fio']) {
+  if ($errors['name_empty']) {
     // Удаляем куку, указывая время устаревания в прошлом.
     setcookie('fio_error', '', 100000);
     // Выводим сообщение.
     $messages[] = '<div class="error">Заполните имя.</div>';
   }
+  if ($errors['name_str']) {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('name_error', '', 100000);
+    // Выводим сообщение.
+    $messages[] = '<div class="error">Имя может содержать только буквы и пробелы.</div>';
+  }
+    if ($errors['name_len']) {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('name_error', '', 100000);
+    // Выводим сообщение.
+    $messages[] = '<div class="error">'Имя не должно превышать 150 символов.</div>';
+  }
+  
   // TODO: тут выдать сообщения об ошибках в других полях.
 
   // Складываем предыдущие значения полей в массив, если есть.
   $values = array();
-  $values['fio'] = empty($_COOKIE['fio_value']) ? '' : $_COOKIE['fio_value'];
+  $values['name'] = empty($_COOKIE['name_value']) ? '' : $_COOKIE['name_value'];
   // TODO: аналогично все поля.
 
   // Включаем содержимое файла form.php.
