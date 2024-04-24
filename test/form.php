d40f4d0a652a04293a3432b0c9196f70943a46fc
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-04-24 21:12:20 +0300

    Update form.php

--- test/form.php
+++ test/form.php
@@ -25,7 +25,7 @@ if (!empty($messages)) {
 
     <form action="" method="POST">
       <input name="fio" <?php if ($errors['fio']) {print 'class="error"';} ?> value="<?php print $values['fio']; ?>" />
-      <input type="submit" value="ok" />
+      <input type="submit" name = "ok" value="ok" />
       <input type="submit" name="logout" value="Выход">
     </form>
   </body>
