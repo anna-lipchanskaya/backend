ca7b8b5b90e0883947deb80d51a487b2cdde222c
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-04-27 18:08:02 +0300

    Update index.php

--- test/index.php
+++ test/index.php
@@ -65,7 +65,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $pass = '7848123'; // Заменить на пароль
     $db = new PDO('mysql:host=localhost;dbname=u67440', $user, $pass,
       [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
-    $stmt = $pdo->prepare("SELECT name, phone FROM users WHERE login = :login");
+    $stmt = $db->prepare("SELECT name, phone FROM users WHERE login = :login");
     $stmt->execute(['login' => $_SESSION['login']]);
     $row = $stmt->fetch();
 
