c604a4c245beb1e99a3f276fd638ded2329b0685
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-04-27 20:14:46 +0300

    Update form.php

--- test/form.php
+++ test/form.php
@@ -26,7 +26,22 @@ if (!empty($messages)) {
     <form action="" method="POST">
       <input name="fio" <?php if ($errors['fio']) {print 'class="error"';} ?> value="<?php print $values['fio']; ?>" />
       <input type="submit" name = "button" value="ok" />
-      <input type="submit" name="button" value="exit">
+      <input type="submit" name="button" id = "myButton" value="exit" />
+      <script>
+        document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById("myButton");
+    
+    // Устанавливаем начальное состояние кнопки
+    button.style.display = "none";
+    
+    // Проверяем условие (например, что переменная isConditionMet равна true)
+    var isConditionMet = true;
+    
+    if (isConditionMet) {
+        button.style.display = "block";
+    }
+});
+      </script>
     </form>
   </body>
 </html>
