13043a43be67ff9c28ab3fe0b9af942b64988d73
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-04-27 20:41:46 +0300

    Create form.php

---
+++ task5/form.php
@@ -0,0 +1,41 @@
+<html>
+  <head>
+    <style>
+/* Сообщения об ошибках и поля с ошибками выводим с красным бордюром. */
+.error {
+  border: 2px solid red;
+}
+    </style>
+  </head>
+  <body>
+
+<?php
+if (!empty($messages)) {
+  print('<div id="messages">');
+  // Выводим все сообщения.
+  foreach ($messages as $message) {
+    print($message);
+  }
+  print('</div>');
+}
+
+// Далее выводим форму отмечая элементы с ошибками классом error
+// и задавая начальные значения элементов ранее сохраненными.
+?>
+
+    <form action="" method="POST">
+      <input name="fio" <?php if ($errors['fio']) {print 'class="error"';} ?> value="<?php print $values['fio']; ?>" />
+      <input type="submit" name = "button" value="ok" />
+      <input type="submit" name="button" id = "myButton" value="exit" />
+      <script>
+        document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById("myButton");
+    button.style.display = "none";
+    if (true) {
+        button.style.display = "block";
+    }
+});
+      </script>
+    </form>
+  </body>
+</html>
