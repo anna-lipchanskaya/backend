bcd1f0f4affd18439531dc3c3e315c7795ef8520
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-04-27 16:41:10 +0300

    Update login.php

--- test/login.php
+++ test/login.php
@@ -20,6 +20,7 @@ if (session_start() && !empty($_COOKIE[session_name()])) {
     if (!empty($_SESSION['login'])) {
         // Если есть логин в сессии, то пользователь уже авторизован.
         if (!empty($_COOKIE['logout'])) {
+            setcookie('logout', '', 100000);
             // Выход пользователя из сессии
             session_destroy();
             header('Location: ./');
