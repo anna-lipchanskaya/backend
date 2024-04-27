64b592d6a55500376ce36ebbe95dcd01470ccc55
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-04-27 19:53:01 +0300

    Update login.php

--- test/login.php
+++ test/login.php
@@ -7,6 +7,19 @@
 }
     </style>
   </head>
+  <?php
+if (!empty($messages)) {
+  print('<div id="messages">');
+  // Выводим все сообщения.
+  foreach ($messages as $message) {
+    print($message);
+  }
+  print('</div>');
+}
+
+// Далее выводим форму отмечая элементы с ошибками классом error
+// и задавая начальные значения элементов ранее сохраненными.
+?>
 <?php
 
 /**
