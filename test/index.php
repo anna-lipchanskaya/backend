2cb172b0a11b96a5d692011c9d4bc1f2a38bc0ec
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-04-27 18:33:43 +0300

    Update index.php

--- test/index.php
+++ test/index.php
@@ -56,7 +56,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
   // Если нет предыдущих ошибок ввода, есть кука сессии, начали сессию и
   // ранее в сессию записан факт успешного логина.
-  if (session_start() && (!empty($_SESSION['login'])) && (!empty($_COOKIE[session_name()]))) {
+  if (session_start() && (!empty($_SESSION['login'])) && (!empty($_COOKIE[session_name()])) && (empty($errors))) {
     // TODO: загрузить данные пользователя из БД
     // и заполнить переменную $values,
     // предварительно санитизовав.
