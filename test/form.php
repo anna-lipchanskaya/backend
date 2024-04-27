f5ffc451e4f26225a6d16b6b106a34658089c30a
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-04-27 15:33:54 +0300

    Update form.php

--- test/form.php
+++ test/form.php
@@ -29,6 +29,7 @@ if (!empty($messages)) {
       <input type="submit" name="logout" value="Выход">
       <?php
       if (isset($_POST['logout'])) {
+        echo "NO";
         $_SESSION['logout'] = $_POST['logout'];
           header('Location: login.php');
 
