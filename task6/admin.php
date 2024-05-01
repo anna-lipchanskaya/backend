ee9f2b499e23ec4a62a919d766d7fa8913190561
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-05-01 13:31:34 +0300

    Update admin.php

--- task6/admin.php
+++ task6/admin.php
@@ -23,7 +23,27 @@ print('Вы успешно авторизовались и видите защи
  include('../db.php');
 $db = new PDO('mysql:host=localhost;dbname=' . $db_name, $db_login, $db_pass,
   [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
+try {
+    $db = new PDO('mysql:host=localhost;dbname=' . $db_name, $db_login, $db_pass, [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
+    $query = $db->query("SELECT name, phone, email, data, pol, bio, ok FROM application3");
+    $results = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    // Вывод данных
+    echo "Пользователь";
+    foreach ($results as $row) {
+        echo "Name: " . $row['name'] . "<br>";
+        echo "Phone: " . $row['phone'] . "<br>";
+        echo "Email: " . $row['email'] . "<br>";
+        echo "Data: " . $row['data'] . "<br>";
+        echo "Gender: " . $row['pol'] . "<br>";
+        echo "Bio: " . $row['bio'] . "<br>";
+        echo "Ok: " . $row['ok'] . "<br><br>";
+    }
+
+} catch (PDOException $e) {
+    echo 'Ошибка: ' . $e->getMessage();
+}
 // *********
 // Здесь нужно прочитать отправленные ранее пользователями данные и вывести в таблицу.
 // Реализовать просмотр и удаление всех данных.
