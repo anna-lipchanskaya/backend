a05867dfa785b5ee711136f26db1b6c10be31d40
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-05-05 17:21:59 +0300

    Update auth.php

--- task6/auth.php
+++ task6/auth.php
@@ -1,16 +1,26 @@
 <?php
-require_once('db.php');   
+// auth.php
+include('../db.php');
+$db = new PDO('mysql:host=localhost;dbname=' . $db_name, $db_login, $db_pass,
+  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
 function checkAuth() {
-        $query = "SELECT login, password FROM admin";
-        $row = executeQuery($query);
-    
-    if (empty($_SERVER['PHP_AUTH_USER']) ||
-        empty($_SERVER['PHP_AUTH_PW']) ||
-        $_SERVER['PHP_AUTH_USER'] != $row["login"] ||
-        !password_verify($_SERVER['PHP_AUTH_PW'], $row["password"])) {
-      header('HTTP/1.1 401 Unauthorized');
-      header('WWW-Authenticate: Basic realm="My site"');
-      exit('<h1>401 Требуется авторизация</h1>');
+    global $db;
+    try {
+        $result = $db->query("SELECT login, password FROM admin");
+        $row = $result->fetch(PDO::FETCH_ASSOC);
+        if (empty($_SERVER['PHP_AUTH_USER']) ||
+            empty($_SERVER['PHP_AUTH_PW']) ||
+            $_SERVER['PHP_AUTH_USER'] != $row["login"] ||
+            !password_verify($_SERVER['PHP_AUTH_PW'], $row["password"])) {
+            header('HTTP/1.1 401 Unauthorized');
+            header('WWW-Authenticate: Basic realm="My site"');
+            exit('<h1>401 Требуется авторизация</h1>');
+        }
+    } catch(PDOException $e) {
+        exit('Error : ' . $e->getMessage());
     }
 }
+
+checkAuth();
 ?>
