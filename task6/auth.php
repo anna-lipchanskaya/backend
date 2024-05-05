0ba54df0e769f87b90b60db26f229c583758ae0a
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-05-05 22:30:16 +0300

    Update auth.php

--- task6/auth.php
+++ task6/auth.php
@@ -2,17 +2,16 @@
 session_start(); // Начало сессии
 require_once('db.php');   
 function checkAuth() {
-        $query = "SELECT login, password FROM admin";
-        $row = db_row(Query($query));
-    
-    if (empty($_SERVER['PHP_AUTH_USER']) ||
-        empty($_SERVER['PHP_AUTH_PW']) ||
-        $_SERVER['PHP_AUTH_USER'] != $row["login"] ||
-        !password_verify($_SERVER['PHP_AUTH_PW'], $row["password"])) {
-      header('HTTP/1.1 401 Unauthorized');
-      header('WWW-Authenticate: Basic realm="My site"');
-      exit('<h1>401 Требуется авторизация</h1>');
-    }
+ $row = db_get_Pass_Login();
+if (empty($_SERVER['PHP_AUTH_USER']) ||
+    empty($_SERVER['PHP_AUTH_PW']) ||
+    $_SERVER['PHP_AUTH_USER'] != $row["login"] ||
+    password_verify($_SERVER['PHP_AUTH_PW'], $row["password"])) {
+  header('HTTP/1.1 401 Unanthorized');
+  header('WWW-Authenticate: Basic realm="My site"');
+  print('<h1>401 Требуется авторизация</h1>');
+  exit();
+}
 }
 checkAuth();
 ?>
