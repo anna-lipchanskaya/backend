180cf2ff803ad76c1ed9acccc7e087f3bf986cc8
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-05-05 21:07:41 +0300

    Update db.php

--- task6/db.php
+++ task6/db.php
@@ -84,6 +84,22 @@ function db_get_Alluser($default = FALSE) {
   }
 }
 
+function db_get_StatusLanguage($default = FALSE) {
+  $query = "SELECT a.userid, a.name, a.phone, a.email, a.data, a.pol, a.bio, a.ok, u.login, GROUP_CONCAT(DISTINCT l2.name SEPARATOR ', ') as languages
+                        FROM application3 a
+                        INNER JOIN users u ON a.userid = u.userid
+                        LEFT JOIN ap_lan3 al3 ON a.userid = al3.userid
+                        LEFT JOIN language2 l2 ON al3.id_language = l2.id
+                        GROUP BY a.userid, a.name, a.phone, a.email, a.data, a.pol, a.bio, a.ok, u.login";
+  $value = executeQuery($query);
+  if (!$value) {
+    return $default;
+  }
+  else {
+    return $value;
+  }
+}
+
 function db_set($name, $value) {
   if (strlen($name) == 0) {
     return;
