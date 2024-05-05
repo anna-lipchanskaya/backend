0639f12790494d4151248f578bece861eb307f3c
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-05-05 17:38:16 +0300

    Update db.php

--- task6/db.php
+++ task6/db.php
@@ -3,13 +3,16 @@ include('../db.php');
 global $db;
 $db = new PDO('mysql:host=localhost;dbname=' . $db_name, $db_login, $db_pass,
   [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
+function db_row($stmt) {
+  return $stmt->fetch(PDO::FETCH_ASSOC);
+}
     function executeQuery($query) {
     global $db;
     try {
         $result = $db->query($query);
         if ($result) {
             // Запрос успешно выполнен
-            return $result->fetchAll(PDO::FETCH_ASSOC);
+            return db_row($result);
         } else {
             // Запрос не удался, логируем ошибку
             logError(db_error());
