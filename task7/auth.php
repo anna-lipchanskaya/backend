1f391dffcfb54af2712ab970ee53a9c043f86cde
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-05-14 11:12:20 +0300

    Create auth.php

---
+++ task7/auth.php
@@ -0,0 +1,16 @@
+<?php
+require_once('db.php');   
+function checkAuth() {
+ $row = db_get_Pass_Login();
+if (empty($_SERVER['PHP_AUTH_USER']) ||
+    empty($_SERVER['PHP_AUTH_PW']) ||
+    $_SERVER['PHP_AUTH_USER'] != $row["login"] ||
+    password_verify($_SERVER['PHP_AUTH_PW'], $row["password"])) {
+  header('HTTP/1.1 401 Unanthorized');
+  header('WWW-Authenticate: Basic realm="My site"');
+  print('<h1>401 Требуется авторизация</h1>');
+  exit();
+}
+}
+checkAuth();
+?>
