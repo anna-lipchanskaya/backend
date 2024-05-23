64cb769e3c2882953f3ffaf54f875e4bdf1d126a
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-05-23 21:41:25 +0300

    Update form.php

--- task7/form.php
+++ task7/form.php
@@ -140,11 +140,12 @@ if (!empty($messages)) {
   }
   print('</div>');
 }
+  $token = $_SESSION['csrf_token'];
 
 // Далее выводим форму отмечая элементы с ошибками классом error
 // и задавая начальные значения элементов ранее сохраненными.
 ?>
-     <input type="hidden" name="csrf_token" value="<?php echo $csrfToken; ?>">
+<input type="hidden" name="csrf_token" value="<?= $token; ?>">
     <label>
       ФИО:<br />
       <input 
