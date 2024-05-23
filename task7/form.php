22a61ac50f191c31a3d8dd6e20de2937bb6b879d
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-05-23 20:25:36 +0300

    Update form.php

--- task7/form.php
+++ task7/form.php
@@ -144,7 +144,7 @@ if (!empty($messages)) {
 // Далее выводим форму отмечая элементы с ошибками классом error
 // и задавая начальные значения элементов ранее сохраненными.
 ?>
-  <input type="hidden" name="csrf_token" value="<?= $token; ?>
+ <input type="hidden" name="csrf_token" value="<?= $token; ?>">
     <label>
       ФИО:<br />
       <input 
