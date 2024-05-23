fdd2f967422e3aa4d94c227a832ff929167fabd9
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-05-23 20:17:37 +0300

    Update login.php

--- task7/login.php
+++ task7/login.php
@@ -157,6 +157,10 @@ else
   $_SESSION['login'] = $login;
   // Записываем ID пользователя.
     $_SESSION['uid'] = $use['userid'];
+//Генерируем CSRF-токен
+      if (!isset($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
 
   // Делаем перенаправление.
   header('Location: ./');
